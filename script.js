a767197f4ed0eeff1939e5430d895c09daea678e
HIKI <119759514+HIKIaqui@users.noreply.github.com>
2026-05-12 21:53:30 -0300

    header fix

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ const topButton = document.getElementById("topButton");
 function updateActiveLink() {
   let currentSection = "";
 
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop - 120;
+  const bottomReached =
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 5;
 
-    if (window.scrollY >= sectionTop) {
-      currentSection = section.getAttribute("id");
-    }
-  });
+  if (bottomReached) {
+    currentSection = sections[sections.length - 1].getAttribute("id");
+  } else {
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 160;
+      const sectionBottom = sectionTop + section.offsetHeight;
+
+      if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+  }
 
   links.forEach(link => {
     link.classList.remove("active");
